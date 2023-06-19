36dd0e666d163871ecdd19f2f5269cf4acc8e9ef
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-06-19 11:26:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
-
+import "./App.css";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";//For getting bootstrap on every component
 function App() {
   return (
     <div className="App">
-      
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <>
+      <div className="header text-center">
+        <h1>TodoList</h1>
+        <button className="btn btn-primary">Create Task</button>
+      </div>
+      <div className="task-container"> 
+
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
