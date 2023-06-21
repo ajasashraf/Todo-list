2155162be2f662911e8a1d079a6c21541a677ceb
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-06-21 23:58:31 +0530

    commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -38,7 +38,7 @@ const TodoList = () => {
     <>
       <div className="header text-center">
         <h3>Todo List</h3>
-        <button className="btn btn btn-outline-dark mt-2"  onClick={() => setModal(true)}>
+        <button className="btn btn btn-outline-dark border-2 mt-2"  onClick={() => setModal(true)}>
           Create Task
         </button>
       </div>
